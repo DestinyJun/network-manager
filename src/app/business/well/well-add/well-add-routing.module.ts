2da3c3afbaa34c84ff1847e99f5c39be86fd23c5
AssassinMike <1787028816@qq.com>
2018-08-14 13:54:25 +0800

    删除井添加模块下的四个子组件：井盖，进井，出井，传感器。现在单用well-add 组件完成井的详情添加。

--- src/app/business/well/well-add/well-add-routing.module.ts
+++ src/app/business/well/well-add/well-add-routing.module.ts
@@ -1,20 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {WellAddComponent} from './well-add.component';
-import {WellEnterInfoComponent} from './well-enter-info/well-enter-info.component';
-import {WellOutInfoComponent} from './well-out-info/well-out-info.component';
-import {WellSensorInfoComponent} from './well-sensor-info/well-sensor-info.component';
-import {WellCoverComponent} from './well-cover/well-cover.component';
 
 const routes: Routes = [
-  {path: '', component: WellAddComponent,
-    children: [
-      {path: 'cover', component: WellCoverComponent},
-      {path: 'enter', component: WellEnterInfoComponent},
-      {path: 'out', component: WellOutInfoComponent},
-      {path: 'sensor', component: WellSensorInfoComponent},
-    ]
-  }
+  {path: '', component: WellAddComponent}
 ];
 
 @NgModule({
--- src/app/business/well/well-add/well-add.module.ts
+++ src/app/business/well/well-add/well-add.module.ts
@@ -3,10 +3,6 @@ import { CommonModule } from '@angular/common';
 import { WellAddRoutingModule } from './well-add-routing.module';
 import { WellAddComponent } from './well-add.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { WellEnterInfoComponent } from './well-enter-info/well-enter-info.component';
-import { WellOutInfoComponent } from './well-out-info/well-out-info.component';
-import { WellSensorInfoComponent } from './well-sensor-info/well-sensor-info.component';
-import {WellCoverComponent} from './well-cover/well-cover.component';
 import {ModalModule} from 'ngx-bootstrap';
 
 @NgModule({
@@ -18,11 +14,7 @@ import {ModalModule} from 'ngx-bootstrap';
     ModalModule.forRoot(),
   ],
   declarations: [
-    WellAddComponent,
-    WellCoverComponent,
-    WellEnterInfoComponent,
-    WellOutInfoComponent,
-    WellSensorInfoComponent
+    WellAddComponent
   ]
 })
 export class WellAddModule { }
